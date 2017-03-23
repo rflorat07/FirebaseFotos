88d6e4d4ddb10b781317392f79cc7dd4adfa70d0
Roger Florat <rflorat@cidiverte.it>
2017-03-23 11:57:12 +0100

    Config App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Config
+import { firebaseConfig } from './config/firebase.config';
+
+// services
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+// Components
 import { AppComponent } from './app.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { FotosComponent } from './components/fotos/fotos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargaComponent,
+    FotosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [CargaImagenesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,8 @@
+// Must export the config
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCaCsAf-l1akS0xBKin-myM42-NzyR3lDg',
+    authDomain: 'fir-fotos.firebaseapp.com',
+    databaseURL: 'https://fir-fotos.firebaseio.com',
+    storageBucket: 'fir-fotos.appspot.com',
+    messagingSenderId: '746730644594'
+};

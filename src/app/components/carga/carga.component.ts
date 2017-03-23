1b2925e410f95fd429a63d24d4a30ad441a17bc1
Roger Florat <rflorat@cidiverte.it>
2017-03-23 12:30:25 +0100

    Cargar Html

--- src/app/components/carga/carga.component.ts
+++ src/app/components/carga/carga.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { CargaImagenesService } from './../../services/carga-imagenes.service';
+
+import { FileItem } from './../../models/file-items';
 
 @Component({
   selector: 'app-carga',
   templateUrl: './carga.component.html',
   styles: []
 })
-export class CargaComponent implements OnInit {
+export class CargaComponent {
 
-  constructor() { }
+  estaSobreDropZone: boolean = false;
+  permitirCargar: boolean = true;
 
-  ngOnInit() {
+  archivos: FileItem[] = [];
+
+  constructor(public _cargaImagenes: CargaImagenesService) { }
+
+  cargarImagenesFirebase() {
+    this.permitirCargar = false;
+    this._cargaImagenes.cargar_imagenes_firebase(this.archivos);
+  }
+
+  limpiarArchivos() {
+    this.archivos = [];
+    this.permitirCargar = true;
   }
 
 }
--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import { FileItem } from './../models/file-items';
+
+import * as firebase from 'firebase';
 
 @Injectable()
 export class CargaImagenesService {
 
-  constructor() { }
+  private CARPETA_IMAGENES: string = 'img';
+
+  constructor(public af: AngularFire) { }
+
+  listaUltimasImagenes(numeroImagenes: number): FirebaseListObservable<any[]> {
+    return this.af.database.list(`/${this.CARPETA_IMAGENES}`, {
+      query: {
+        limitToLast: numeroImagenes
+      }
+    });
+  }
+
+  cargar_imagenes_firebase(archivos: FileItem[]) {
+    console.log(archivos);
+  }
+
+  private guardarImagen(imagen: any) {
+    this.af.database.list(`/${this.CARPETA_IMAGENES}`).push(imagen);
+  }
 
 }
